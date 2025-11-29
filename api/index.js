9d42857734b04ff2f6bdbdbd90c4171609bab65f
Yash7801 <yashfine0099@gmail.com>
2025-11-30 02:20:26 +0530

    try 4

--- api/index.js
+++ api/index.js
@@ -10,13 +10,29 @@ import authRouter from "./routes/auth.js";
 
 const app = express();
 
-// SIMPLE + SAFE CORS
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-  ],
-  credentials: true,
-}));
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://axelblaze.vercel.app"
+];
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
+
 
 app.use(express.json());
 app.use(cookieParser());
