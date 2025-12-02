621374f47fa2c6b3ba6294eedc827032705176e7
Yash7801 <yashfine0099@gmail.com>
2025-12-02 15:37:53 +0530

    try47

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from "../contexts/authContext";
+import React from "react";
 
 export default function ProtectedRoute({ children }) {
   const { currentUser } = useContext(AuthContext);
