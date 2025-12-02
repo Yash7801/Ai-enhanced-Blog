941a98185b8f7aeeaf23005b94e24fb0ecac9654
Yash7801 <yashfine0099@gmail.com>
2025-12-02 16:46:31 +0530

    try51

--- client/src/api.js
+++ client/src/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  withCredentials: true
+  withCredentials: true,
 });
+
+axiosInstance.defaults.withCredentials = true;
 
 export default axiosInstance;
